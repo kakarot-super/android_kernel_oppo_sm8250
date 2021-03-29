e3d3e1e2a12768a4f234b457cac18957636c55c6
Gao Xiang <hsiangkao@redhat.com>
2021-03-29 09:23:07 +0800

    erofs: introduce on-disk lz4 fs configurations
    Introduce z_erofs_lz4_cfgs to store all lz4 configurations.
    Currently it's only max_distance, but will be used for new
    features later.
    
    Link: https://lore.kernel.org/r/20210329012308.28743-4-hsiangkao@aol.com
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Gao Xiang <hsiangkao@redhat.com>
    Change-Id: Ic6db91a5075cfb788b721ffef41c27d144a69602

--- fs/erofs/super.c
+++ fs/erofs/super.c
@@ -169,7 +169,7 @@ static int erofs_read_superblock(struct super_block *sb)
 	}
 
 	/* parse on-disk compression configurations */
-	ret = z_erofs_load_lz4_config(sb, dsb);
+	ret = z_erofs_load_lz4_config(sb, dsb, NULL, 0);
 out:
 	kunmap_atomic(data);
 	put_page(page);
