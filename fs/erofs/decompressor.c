79eb73f2d808c5fe03348f6cf5d599089ce65a11
Gao Xiang <hsiangkao@linux.alibaba.com>
2021-10-25 15:43:11 +0800

    erofs: don't trigger WARN() when decompression fails
    [ Upstream commit a0961f351d82d43ab0b845304caa235dfe249ae9 ]
    
    syzbot reported a WARNING [1] due to corrupted compressed data.
    
    As Dmitry said, "If this is not a kernel bug, then the code should
    not use WARN. WARN if for kernel bugs and is recognized as such by
    all testing systems and humans."
    
    [1] https://lore.kernel.org/r/000000000000b3586105cf0ff45e@google.com
    
    Link:
    https: //lore.kernel.org/r/20211025074311.130395-1-hsiangkao@linux.alibaba.com
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Reported-by: syzbot+d8aaffc3719597e8cfb4@syzkaller.appspotmail.com
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Change-Id: I0b565575dc04765e3330a2e82bcc9c071106df45

--- fs/erofs/decompressor.c
+++ fs/erofs/decompressor.c
@@ -177,16 +177,10 @@ static int z_erofs_lz4_decompress(struct z_erofs_decompress_req *rq, u8 *out)
     }
 #endif
 
-    if (ret < 0) {
-        erofs_err(rq->sb, "failed to decompress, in[%u, %u] out[%u]",
-              inlen, inputmargin, rq->outputsize);
-        WARN_ON(1);
-        print_hex_dump(KERN_DEBUG, "[ in]: ", DUMP_PREFIX_OFFSET,
-                   16, 1, src + inputmargin, inlen, true);
-        print_hex_dump(KERN_DEBUG, "[out]: ", DUMP_PREFIX_OFFSET,
-                   16, 1, out, rq->outputsize, true);
-        ret = -EIO;
-    }
+		print_hex_dump(KERN_DEBUG, "[ in]: ", DUMP_PREFIX_OFFSET,
+			       16, 1, src + inputmargin, inlen, true);
+		print_hex_dump(KERN_DEBUG, "[out]: ", DUMP_PREFIX_OFFSET,
+			       16, 1, out, rq->outputsize, true);
 
     if (copied)
         erofs_put_pcpubuf(src);
