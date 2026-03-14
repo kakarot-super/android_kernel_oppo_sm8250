c1ce1507dabf73bd07b82c59725d6f5305d94ebe
kakarot-super <kakarot@example.com>
2026-03-14 11:11:20 +0800

    block: optimize IO latency and prioritize swap requests for UI smoothness

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -5452,6 +5452,8 @@ static void shrink_node_memcg(struct pglist_data *pgdat, struct mem_cgroup *memc
 
 				nr_reclaimed += shrink_list(lru, nr_to_scan,
 							    lruvec, sc);
+				if (unlikely(sc->priority < DEF_PRIORITY - 2))
+                    cond_resched();
 			}
 		}
 
